b4f9c74267e9b8ea34d0ebfe4f7b0befb1b57f38
chai-hf <193466761+chai-hf@users.noreply.github.com>
2025-04-06 14:41:13 -0700

    add config files

---
+++ pre_bench.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+mount -t tmpfs -o size=8g none build
+mount -t tmpfs -o size=8g none analysis
+mount -t tmpfs -o size=8g none problems
+echo 0 > /proc/sys/kernel/randomize_va_space
+echo 0 > /proc/sys/kernel/perf_event_paranoid
+echo 1 > /sys/devices/system/cpu/intel_pstate/no_turbo
+for i in /sys/devices/system/cpu/cpu*/cpufreq/scaling_governor;do echo performance > $i;done
